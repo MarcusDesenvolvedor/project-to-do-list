ef9bd72ca0671be10fcad0139d15b1be0c318df8
Marcus Antonio Ribeiro da Silva <marcussilva15@outlook.com.br>
2023-02-23 14:26:26 -0300

    Creation of components and functionality

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
+  public taskList: Array<TaskList> = [];
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.taskList = [
+      {
+        task: "New task 1",
+        checked: true
+      },
+      {
+        task: "New task 2",
+        checked: false
+      }
+    ]
+  }
 }
